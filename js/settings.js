eeed1c162872b8352a0c05daf238085095555896
gaurav-verkada <85907198+gaurav-verkada@users.noreply.github.com>
2022-01-02 21:29:47 -0800

    cleanup

--- js/settings.js
+++ js/settings.js
@@ -1,27 +1,17 @@
-document.querySelectorAll(".pl").forEach((item) =>
-  item.addEventListener("click", () => {
-    items = document.querySelectorAll(".active-pl");
-    for (const e of items) e.classList.remove("active-pl");
-    item.classList.add("active-pl");
-    const time = item.id.slice(3);
+["pl", "sb"].map((cl) => {
+  document.querySelectorAll(`.${cl}`).forEach((item) =>
+    item.addEventListener("click", () => {
+      console.log(item);
+      document.querySelectorAll(`.active-${cl}`).forEach((e) => {
+        e.classList.remove(`active-${cl}`);
+      });
+      item.classList.add(`active-${cl}`);
 
-    if (window.localStorage) {
-      localStorage.setItem("plTime", `${time}:00`);
-      localStorage.setItem("plItem", `#${item.id}`);
-    }
-  })
-);
-
-document.querySelectorAll(".sl").forEach((item) =>
-  item.addEventListener("click", () => {
-    items = document.querySelectorAll(".active-sl");
-    for (const e of items) e.classList.remove("active-sl");
-    item.classList.add("active-sl");
-
-    const time = item.id.slice(3);
-    if (window.localStorage) {
-      localStorage.setItem("sbTime", `${time}:00`);
-      localStorage.setItem("sbItem", `#${item.id}`);
-    }
-  })
-);
+      const time = item.id.slice(3);
+      if (window.localStorage) {
+        localStorage.setItem(`${cl}Time`, `${time}:00`);
+        localStorage.setItem(`${cl}Item`, `#${item.id}`);
+      }
+    })
+  );
+});
